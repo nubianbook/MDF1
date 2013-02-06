77ea68ba817d48959e0bafd074ae748bfea825a7
nubianbook <nubianbook@me.com>
2013-02-06 14:59:32 -0500

    I am updating my files with a custom view

---
+++ MDF1-Project1/MDF1-Project1/CustomTableCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableCell.h
+//  MDF1-Project1
+//
+//  Created by Carol Gaylor on 2/6/13.
+//  Copyright (c) 2013 Carol Gaylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableCell : UITableViewCell
+
+@end
---
+++ MDF1-Project1/MDF1-Project1/CustomTableCell.m
@@ -0,0 +1,29 @@
+//
+//  CustomTableCell.m
+//  MDF1-Project1
+//
+//  Created by Carol Gaylor on 2/6/13.
+//  Copyright (c) 2013 Carol Gaylor. All rights reserved.
+//
+
+#import "CustomTableCell.h"
+
+@implementation CustomTableCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
