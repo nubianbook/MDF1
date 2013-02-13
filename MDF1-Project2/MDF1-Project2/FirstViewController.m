c5b65b65a9c80ce17a766b6cddf2dc7762226fdf
nubianbook <nubianbook@me.com>
2013-02-13 18:10:08 -0500

    I am updating my detail View

--- MDF1-Project2/MDF1-Project2/FirstViewController.m
+++ MDF1-Project2/MDF1-Project2/FirstViewController.m
@@ -39,9 +39,9 @@
 {
     stringArray = [[NSMutableArray alloc] initWithObjects:@"String Beans", @"Squash", nil];
     stringDetailArray = [[NSMutableArray alloc] initWithObjects:@"1. Wait for warmth:  Beans like warm soil and air. 2.	Prepare your seeds:  Soak seeds in water for 30 minutes before planting for better germination. 3.	Water wisely:  Beans need an adequate and regular supply of water. 4.	Plant them snug:  Crowding beans close together can still yield a good crop. 5.	Rotate! Rotating crops every year help prevent certain crop diseases. 6. Weed!  Beans have shallow roots, that can quickly lose a fight for moisture and nutrients to weeds. 7.	Mulch!  Mulch help keep moisture around the shallow roots during the hot summer months 8. Pick often!  Regular picking is the number one way to prolong a bean harvest. 9. Be Cool!  If you notice a stall in the bean harvest after a hot spell no worries as the heat cool off the plants will re – pollinate themselves.  10.	Plant in waves:  Plant 10 to 15 foot long rows every two to three weeks to have beans all throughout the season.", @"Yellow squash are buttery yellow, some with elongated necks others with crooked neck.                                                    Zucchini squash produce large crops of club shaped fruits.                                               These can be seen in various shades of deep yellows and greens.                                                                                                                 Round and oval squash sometimes produce small compact individual serving size fruit.                                                              Pattypan squash produce large crops of plump scalloped edged, flyer saucer looking fruits that can be found in stripped bright yellow.                                                                                   The best time to plant is in the spring an after the frost.  And try to plant them in raised beds or prepared hills.  Plant seeds at least 8 inches apart", nil];
-    stringNameArray = [[NSMutableArray alloc] initWithObjects:@"10 String Bean Tips", @"Squash Types to Try & Tips", nil];
+    stringNameArray = [[NSMutableArray alloc] initWithObjects:@"10. String Bean Tips", @"Squash Types to Try & Tips", nil];
     stringinfoArray = [[NSMutableArray alloc] initWithObjects:@"Harrar, S. (1995). Super easy string beans. Organic Gardening (08973792), 42(5), 54.", @"Pleasant, B. (2012). All About SUMMER SQUASH. Mother Earth News, (252), 70-72.", nil];
-    stringURLArray = [[NSMutableArray alloc] initWithObjects:@"", @"http://search.ebscohost.com.oclc.fullsail.edu:81/login.aspx?direct=true&db=a9h&AN=75333336&site=ehost-live", nil];
+    stringURLArray = [[NSMutableArray alloc] initWithObjects:@"http://search.ebscohost.com.oclc.fullsail.edu:81/login.aspx?direct=true&db=a9h&AN=9504300359&site=ehost-live", @"http://search.ebscohost.com.oclc.fullsail.edu:81/login.aspx?direct=true&db=a9h&AN=75333336&site=ehost-live", nil];
     
     
     [super viewDidLoad];
@@ -91,6 +91,7 @@
         myDetailController.detailLabel.text= (NSString*)[stringDetailArray objectAtIndex:indexPath.row];
         myDetailController.nameLabel.text= (NSString*)[stringNameArray objectAtIndex:indexPath.row];
         myDetailController.infoLabel.text= (NSString*)[stringinfoArray objectAtIndex:indexPath.row];
+        myDetailController.urlLabel.text= (NSString*)[stringURLArray objectAtIndex:indexPath.row];
     }
 }
 
